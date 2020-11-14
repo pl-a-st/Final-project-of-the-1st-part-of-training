2b4636ff89c05aeb3a2b393f0d97c7630f525408
Максим Богащенко <pl_a_st@live.ru>
2020-11-14 11:55:03 +0300

    14/11/20//11/54

---
+++ Final-project-of-the-1st-part-of-training/Final-project-of-the-1st-part-of-training/Resident.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public enum Location
+{
+    home,
+    street
+}
+
+namespace Final_project_of_the_1st_part_of_training
+{
+    public class Resident
+    {
+        public Location Location
+        { get; private set;}
+
+    }
+}
