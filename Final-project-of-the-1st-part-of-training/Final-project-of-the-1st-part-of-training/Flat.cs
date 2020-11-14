613420c90bd6f145f3113802be9544c15731686c
Максим Богащенко <pl_a_st@live.ru>
2020-11-14 11:21:17 +0300

    14/11/20//11/21

---
+++ Final-project-of-the-1st-part-of-training/Final-project-of-the-1st-part-of-training/Flat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Final_project_of_the_1st_part_of_training
+{
+    public class Flat
+    {
+        public int Floor
+        { get; private set; }
+        public int Capacity
+        { get; private set; }
+        public int ResidentCount
+        { get; private set; }
+        public int Number
+        { get; private set;}
+        public Flat(int floor, int capacity,int number)
+        {
+            Floor = floor;
+            Capacity = capacity;
+            Number = number;
+        }
+        public void AddResident()
+        {
+            if (Capacity==ResidentCount)
+            {
+                Console.WriteLine("Вместимость квартиры не позволяет добавить еще одного человека.");
+                Console.Write("Для продолжения нажмите любую кнопку: ");
+                Console.ReadLine();
+            }
+            else
+            {
+                ResidentCount++;
+            }
+        }
+    }
+}
---
+++ Final-project-of-the-1st-part-of-training/Final-project-of-the-1st-part-of-training/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Final_project_of_the_1st_part_of_training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
