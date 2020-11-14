4ff598018f3cb65f2ab1ed4b8179fc53c0f13c78
Максим Богащенко <pl_a_st@live.ru>
2020-11-14 19:12:43 +0300

    19/12/20//1912

--- Final-project-of-the-1st-part-of-training/Final-project-of-the-1st-part-of-training/Resident.cs
+++ Final-project-of-the-1st-part-of-training/Final-project-of-the-1st-part-of-training/Resident.cs
@@ -11,8 +11,24 @@ namespace Final_project_of_the_1st_part_of_training
 {
     public class Resident
     {
+        public string Name
+        { get; private set; }
+
         public Location Location
-        { get; private set;}
+        { get; private set; }
+        public int Wt
+        { get; private set; }
+        public int FlatNumber
+        { get; private set; }
+        public Resident(int wt, string name)
+        {
+            Name = name;
+            Wt = wt;
+        }
+        public void PutInAnFlat(int flatNumber)
+        {
+            FlatNumber = flatNumber;
+        }
 
     }
 }
